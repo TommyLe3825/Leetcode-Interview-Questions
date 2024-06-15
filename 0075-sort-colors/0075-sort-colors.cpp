af5ae6676dcc62d38ce6ecf73f413bc8b00444fc
Tommy Le <tommyle1624@gmail.com>
2024-06-15 16:56:10 -0700

    Time: 0 ms (100.00%), Space: 9.9 MB (35.99%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        //Dutch Flag Algorithm (Only works if it's only 0, 1, 2)
+        int low = 0, mid = 0, high = nums.size() - 1;
+        
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else { // nums[mid] == 2
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
