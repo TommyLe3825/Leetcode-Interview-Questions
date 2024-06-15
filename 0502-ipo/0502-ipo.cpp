04724fb69c11798815abfd8d861f9e2ca2029343
Tommy Le <tommyle1624@gmail.com>
2024-06-15 16:54:17 -0700

    Time: 141 ms (62.08%), Space: 81.5 MB (89.28%) - LeetHub

--- 0502-ipo/0502-ipo.cpp
+++ 0502-ipo/0502-ipo.cpp
@@ -5,10 +5,13 @@
 class Solution {
 public:
     int findMaximizedCapital(int k, int w, std::vector<int>& profits, std::vector<int>& capital) {
+        //k = maximum number of projects that can be completed
+        //w = inital capital
         int n = profits.size();
         std::vector<std::pair<int, int>> projects(n);
         
         for (int i = 0; i < n; ++i) {
+            //Paring the capital and profit elements togehter
             projects[i] = {capital[i], profits[i]};
         }
         
@@ -16,14 +19,20 @@ public:
         std::sort(projects.begin(), projects.end());
         
         //Max-heap to store the profits of the projects that can be started
+        //maxHeap stores the highest profit on top always among other feasible projects
         std::priority_queue<int> maxHeap;
         
+        //It will be used to track the current position in the sorted list of projectts
         int projectIndex = 0;
         
         for (int i = 0; i < k; ++i) {
             //Add all projects that can be started with the current capital
+            //ProjectIndex < n makes sure the projectIndex is within the bounds of profits.size
+            //Projects[projectIndex].first... checks if the current capital is <= w
             while (projectIndex < n && projects[projectIndex].first <= w) {
-                maxHeap.push(projects[projectIndex].second);
+                //Pushes current profit to maxHeap
+                maxHeap.push(projects[projectIndex].second); 
+                //Updates the index immediately
                 ++projectIndex;
             }
             
@@ -34,7 +43,7 @@ public:
             
             //Select the project with the maximum profit
             w += maxHeap.top();
-            maxHeap.pop();
+            maxHeap.pop(); //remove it after adding the profit to w
         }
         
         return w;
