c8bcb59b243d664111edf6b65546ca136cfb9a13
Tommy Le <tommyle1624@gmail.com>
2024-06-17 17:09:54 -0700

    Time: 5 ms (34.35%), Space: 7.2 MB (18.55%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -3,8 +3,10 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-        long a = 0;
-        long b = static_cast<long>(sqrt(c));
+        long a = 0; //Starting from the smallest non-negative integer and working up
+        long b = sqrt(c); //Starting from the largest possible non-negative integer and work out way down
+        
+        //We are using a two-pointer approach!
         
         while (a <= b) {
             long sumOfSquares = a * a + b * b;
